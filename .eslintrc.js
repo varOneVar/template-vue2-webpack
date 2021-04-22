432ff42e82403785106a05d47f1f078b26d52b7d
varOneVar <2198093767@qq.com>
2021-04-22 23:14:13 +0800

    v1.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-20 10:53:34
+ * @LastEditTime: 2021-04-22 18:12:54
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \app-test\.eslintrc.js
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended' // 添加 prettier 插件
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    parser: '@babel/eslint-parser'
+  },
+  plugins: ['vue', 'html'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'no-underscore-dangle': 'allow',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? { allow: ['warn', 'error'] } : 0,
+    'vue/order-in-components': [
+      'warn',
+      {
+        order: [
+          'el',
+          'name',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['directives', 'filters'],
+          'extends',
+          'mixins',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'fetch',
+          'asyncData',
+          'data',
+          'methods',
+          'LIFECYCLE_HOOKS',
+          'computed',
+          'watch',
+          'components',
+          'head',
+          ['template', 'render'],
+          'renderError'
+        ]
+      }
+    ]
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="form-class a1sd--213--asdas-123"></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  methods: {
+    testHandler() {
+      console.log(1)
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 80px;
+  padding: 20px;
+  margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.form-class {
+  color: red;
+}
+
+.a1sd--213--asdas-123 {
+  color: blue;
+}
+</style>
